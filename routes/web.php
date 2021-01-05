cbe3b53cb743ad3f9156ee92f37826f151d0ff79
Nurul-ahdt <ahdiatinurul@gmail.com>
2021-01-04 17:31:42 -0800

    Membuat CRUD Data Mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//pages
+Route::get('/', 'PagesController@home');
+
+//students
+Route::resource('students', 'StudentsController');
